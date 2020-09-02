d71de7bf26b4e0c54c7ccc895c1601ef60ff1dec
Corey Martin <coreyjmartin2@gmail.com>
2020-09-02 19:42:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,14 @@
 class Dog 
   
-  attr_accessor :name
+  attr_accessor :name :breed
   
   def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed 
   end
   
-  def breed(breed)
+
+    
     
   
 end 
